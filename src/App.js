0faf53f42216de0ac09462605630aa0a617f4daf
Zoryana Tur <106607876+ZoryanaTur@users.noreply.github.com>
2022-08-31 20:58:36 +0300

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
 import shecodeslogo from "./shecodeslogo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <img
-          src={shecodeslogo}
-          alt="SheCodes logo"
-          className="App-logo img-fluid"
-        />
+        <header>
+          <img
+            src={shecodeslogo}
+            alt="SheCodes logo"
+            className="App-logo img-fluid"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>
+            This App was coded by{" "}
+            <a href="https://idyllic-unicorn-26008a.netlify.app/">
+              Zoryana Tur
+            </a>{" "}
+            and{" "}
+            <a href="https://github.com/ZoryanaTur/dictionary-project">
+              open-sourced on GitHub
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
