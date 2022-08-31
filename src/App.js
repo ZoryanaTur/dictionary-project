41b1df6c3c0135b6823b6250b2287b471b02f8da
Zoryana Tur <106607876+ZoryanaTur@users.noreply.github.com>
2022-08-31 19:37:38 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import shecodeslogo from "./shecodeslogo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img
+          src={shecodeslogo}
+          alt="SheCodes logo"
+          className="App-logo img-fluid"
+        />
+      </div>
+    </div>
+  );
+}
